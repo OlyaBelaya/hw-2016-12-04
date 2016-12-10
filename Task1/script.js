8c7740b5670af3f1a133936072cb36fadd30adce
Olya Belaya <OlyaBelaya84@gmail.com>
2016-12-10 10:32:09 +0200

    first commit

---
+++ Task1/script.js
@@ -0,0 +1,69 @@
+﻿Date.prototype.formatDate = function(format){
+
+let arrMonth = ["January", "Fabuary", "March", "April", "May", "June", "July", 
+                "August", "September", "October", "November", "Desember"];
+
+let	options = 	{
+	    YYYY : this.getFullYear(),
+		YY   : String(this.getFullYear()).substring(2),
+		MM   : this.getMonth() + 1,
+		MMMM : arrMonth[this.getMonth()],
+		DD   : this.getDate(), 
+		HH   : (String(this.getHours()).length > 1 ? this.getHours() : "0" + this.getHours()),
+		mm   : (String(this.getMinutes()).length > 1 ? this.getMinutes() : "0" + this.getMinutes()), 
+		ss   : (String(this.getSeconds()).length > 1 ? this.getSeconds() : "0" + this.getSeconds()),
+};
+
+let delimiter1 = "-";
+
+let delimiter2 = " ";
+
+let delimiter3 = ":";
+
+let maxPos = 0;
+
+let formatString = "";
+let component = "";
+console.log(format);
+	for (let i = 0; i < format.length; i++){
+		ind1 = format.indexOf(delimiter1, i);
+
+		ind2 = format.indexOf(delimiter2, i);
+
+		ind3 = format.indexOf(delimiter3, i);
+
+		let arrIndex = [ind1, ind2, ind3];
+		let arrClear = arrIndex.filter(item => item > 0 );
+		//console.log(arrClear);
+		if (arrClear.length <= 0) {
+			minPos = format.length;
+			component = format.substring(i);
+		} else {
+			minPos = Math.min.apply(null, arrClear);
+			component = format.substring(i, minPos);
+		}
+
+		//console.log(component);
+		if (!(component in options)) {console.log("No valid component:" + component); return "Error...";}
+
+		formatString = formatString + options[component] + (minPos > 0 ? format.substr(minPos,1) : "");
+		//console.log(formatString);
+
+		i = minPos;
+	}
+
+return console.log(formatString); 
+}
+
+var today = new Date();
+
+console.log(today);
+
+today.formatDate("YYYY-MMMM-DD HH:mm:ss");
+today.formatDate("YYYY-MM-DD HH:mm:ss");//("YYYY-MMMM-DD HH:mm:ss");
+today.formatDate("YYYY-MM-DD HH:mm");
+today.formatDate("YYYY-MM-DD HH");
+today.formatDate("MM-DD HH:mm:ss");
+today.formatDate("DD HH mm:ss");
+today.formatDate("YY MMMM");
+today.formatDate("YYMMMM");
---
+++ Task2/script.js
@@ -0,0 +1,68 @@
+﻿;(function() {
+	"use strict";
+
+    let str = prompt("Enter the random number from 1 to 87", "");
+     
+	let num = Number(str);
+
+ 
+function getData(num) {
+	fetch("http://swapi.co/api/people/" + num + "/")
+	.then(res => res.json())
+	.then(function(res) {
+	//console.log(res.name);
+	     let name = res.name;
+	     let species = res.species;
+		 let films = res.films,
+	         getFilms = films.map(film => fetch(film).then(result => result.json()));
+		return Promise.all(getFilms).then(res => ({name,species,films: res}));
+	})
+	.then(function(res) {
+     let name = res.name;
+     let films = res.films;
+     let species = res.species,
+         getSpecies = species.map(spec => fetch(spec).then(result => result.json()));
+
+
+     return Promise.all(getSpecies).then(res => ({name,films,species: res}));
+
+	})
+	.then(function(res) {
+		//console.log(res);
+		let name = res.name,
+		films    = res.films,
+		species  = res.species,
+		people   = res.species[0].people,
+		getPeople = people.map(peop => fetch(peop).then(result => result.json()));
+
+
+        return Promise.all(getPeople).then(res => ({name,films,species, people: res}));
+})
+	.then(function(res) {
+		let name = res.name,
+		films    = res.films,
+		species  = res.species,
+		people   = res.people;
+
+              alert(`
+			${name}
+			films: ${films.map(film => film.title).join(", ")}
+			Species: ${species.map(spec => spec.name).join(", ")}
+			Language: ${species.map(spec => spec.language).join(", ")}
+			Same origin: ${people.map(peop => peop.name).join(", ")}
+			`);
+               
+	})
+	.catch(function(e) {
+		alert("ERROR!\n You enter the wrong number...\n Try again...");
+		console.log(e);
+	});
+}
+
+ getData(num);
+
+
+
+}) ();
+
+
