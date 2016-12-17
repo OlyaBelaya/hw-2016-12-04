a1b6ef4debba0cd82210af8454b0b16b31000cfa
Olya Belaya <OlyaBelaya84@gmail.com>
2016-12-17 13:36:36 +0200

    New version

--- Task1/script.js
+++ Task1/script.js
@@ -5,52 +5,22 @@ let arrMonth = ["January", "Fabuary", "March", "April", "May", "June", "July",
 
 let	options = 	{
 	    YYYY : this.getFullYear(),
+	    MMMM : arrMonth[this.getMonth()],
 		YY   : String(this.getFullYear()).substring(2),
 		MM   : this.getMonth() + 1,
-		MMMM : arrMonth[this.getMonth()],
 		DD   : this.getDate(), 
 		HH   : (String(this.getHours()).length > 1 ? this.getHours() : "0" + this.getHours()),
 		mm   : (String(this.getMinutes()).length > 1 ? this.getMinutes() : "0" + this.getMinutes()), 
 		ss   : (String(this.getSeconds()).length > 1 ? this.getSeconds() : "0" + this.getSeconds()),
 };
 
-let delimiter1 = "-";
+let formatString = format;
 
-let delimiter2 = " ";
-
-let delimiter3 = ":";
-
-let maxPos = 0;
-
-let formatString = "";
-let component = "";
 console.log(format);
-	for (let i = 0; i < format.length; i++){
-		ind1 = format.indexOf(delimiter1, i);
 
-		ind2 = format.indexOf(delimiter2, i);
-
-		ind3 = format.indexOf(delimiter3, i);
-
-		let arrIndex = [ind1, ind2, ind3];
-		let arrClear = arrIndex.filter(item => item > 0 );
-		//console.log(arrClear);
-		if (arrClear.length <= 0) {
-			minPos = format.length;
-			component = format.substring(i);
-		} else {
-			minPos = Math.min.apply(null, arrClear);
-			component = format.substring(i, minPos);
-		}
-
-		//console.log(component);
-		if (!(component in options)) {console.log("No valid component:" + component); return "Error...";}
-
-		formatString = formatString + options[component] + (minPos > 0 ? format.substr(minPos,1) : "");
-		//console.log(formatString);
-
-		i = minPos;
-	}
+for (let key in options) {
+	formatString = formatString.replace(key, options[key]);
+}
 
 return console.log(formatString); 
 }
@@ -60,8 +30,10 @@ var today = new Date();
 console.log(today);
 
 today.formatDate("YYYY-MMMM-DD HH:mm:ss");
-today.formatDate("YYYY-MM-DD HH:mm:ss");//("YYYY-MMMM-DD HH:mm:ss");
+today.formatDate("YYYY/MMMM/DD HH:mm:ss");
+today.formatDate("YYYY-MM-DD HH:mm:ss");
 today.formatDate("YYYY-MM-DD HH:mm");
+today.formatDate("YYYY_MM_DD HH&mm");
 today.formatDate("YYYY-MM-DD HH");
 today.formatDate("MM-DD HH:mm:ss");
 today.formatDate("DD HH mm:ss");
--- Task2/script.js
+++ Task2/script.js
@@ -1,10 +1,11 @@
-﻿;(function() {
+;(function() {
 	"use strict";
 
     let str = prompt("Enter the random number from 1 to 87", "");
      
 	let num = Number(str);
 
+	if (num < 1 || num > 87) {return alert("You enter the wrong number...\n Try again...");}
  
 function getData(num) {
 	fetch("http://swapi.co/api/people/" + num + "/")
@@ -14,30 +15,29 @@ function getData(num) {
 	     let name = res.name;
 	     let species = res.species;
 		 let films = res.films,
-	         getFilms = films.map(film => fetch(film).then(result => result.json()));
-		return Promise.all(getFilms).then(res => ({name,species,films: res}));
+	         getFilms = films.map(film => fetch(film).then(result => result.json())),
+                 getSpecies = species.map(spec => fetch(spec).then(result => result.json()));
+		return Promise.all(getFilms).then(res => ({name,getSpecies,films: res}));
 	})
 	.then(function(res) {
+     console.log(res);
      let name = res.name;
-     let films = res.films;
-     let species = res.species,
-         getSpecies = species.map(spec => fetch(spec).then(result => result.json()));
+     let films = res.films; 
+     let species = res.getSpecies;
 
-
-     return Promise.all(getSpecies).then(res => ({name,films,species: res}));
+     return Promise.all(species).then(res => ({name,films,species: res}));
 
 	})
 	.then(function(res) {
-		//console.log(res);
+		console.log(res);
 		let name = res.name,
 		films    = res.films,
 		species  = res.species,
 		people   = res.species[0].people,
 		getPeople = people.map(peop => fetch(peop).then(result => result.json()));
 
-
         return Promise.all(getPeople).then(res => ({name,films,species, people: res}));
-})
+	})
 	.then(function(res) {
 		let name = res.name,
 		films    = res.films,
@@ -50,8 +50,7 @@ function getData(num) {
 			Species: ${species.map(spec => spec.name).join(", ")}
 			Language: ${species.map(spec => spec.language).join(", ")}
 			Same origin: ${people.map(peop => peop.name).join(", ")}
-			`);
-               
+			`);       
 	})
 	.catch(function(e) {
 		alert("ERROR!\n You enter the wrong number...\n Try again...");
@@ -61,8 +60,4 @@ function getData(num) {
 
  getData(num);
 
-
-
 }) ();
-
-
